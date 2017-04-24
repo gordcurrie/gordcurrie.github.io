485dcd9335238730d17e3f002029ad4078d014b3
gordon.currie <curriegrr2@gmail.com>
2017-04-24 00:48:28 -0500

    playing around

---
+++ content/first_post.md
@@ -0,0 +1,5 @@
+Title: My First Post
+Date: 2017-04-24 00:35
+Category: General
+
+First Post just to see if everything is working.
---
+++ pelicanconf.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = 'Gord Currie'
+SITENAME = 'Simple Gord'
+SITEURL = ''
+
+PATH = 'content'
+
+TIMEZONE = 'America/Winnipeg'
+
+DEFAULT_LANG = 'en'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Blogroll
+LINKS = (('Pelican', 'http://getpelican.com/'),
+         ('Python.org', 'http://python.org/'),
+         ('Jinja2', 'http://jinja.pocoo.org/'),)
+
+# Social widget
+SOCIAL = (('Twitter', 'https://twitter.com/grrcurrie'),
+          ('LinkedIn', 'https://www.linkedin.com/in/gord-currie-295b9a24/'),)
+
+DEFAULT_PAGINATION = 10
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
